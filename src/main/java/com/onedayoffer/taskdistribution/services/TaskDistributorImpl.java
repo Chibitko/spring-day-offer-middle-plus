74258067fcdebf6c1edd918c158a8cf9ff9b14e7
chibitko <chibitko@mail.ru>
2024-03-23 13:25:22 +0300

    делал тестовое задание

---
+++ src/main/java/com/onedayoffer/taskdistribution/services/TaskDistributorImpl.java
@@ -0,0 +1,48 @@
+package com.onedayoffer.taskdistribution.services;
+
+import com.onedayoffer.taskdistribution.DTO.EmployeeDTO;
+import com.onedayoffer.taskdistribution.DTO.TaskDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+import static java.util.Comparator.comparing;
+
+@Component
+public class TaskDistributorImpl implements TaskDistributor  {
+    private static final Comparator<EmployeeDTO> EMPLOYEE_PRIORITY_COMPARATOR
+            = comparing(EmployeeDTO::getTotalLeadTime);
+
+    private static final Comparator<TaskDTO> TASK_ORDER_COMPARATOR
+            = comparing(TaskDTO::getPriority);
+
+    // TODO рассмотреть модифицированнный алгоритм рюкзака.
+    public void distribute(List<EmployeeDTO> employees, List<TaskDTO> tasks) {
+        // Предусловие.
+        assert employees != null;
+        assert tasks != null;
+
+        if (employees.isEmpty() || tasks.isEmpty()) {
+            // Некому или нечего распределять.
+            return;
+        }
+
+        PriorityQueue<EmployeeDTO> employeePriority = new PriorityQueue<>(EMPLOYEE_PRIORITY_COMPARATOR);
+        employeePriority.addAll(employees);
+
+        List<TaskDTO> orderedTasks = tasks.stream()
+                .sorted(TASK_ORDER_COMPARATOR)
+                .toList();
+
+        for (TaskDTO task : orderedTasks) {
+            EmployeeDTO employee = employeePriority.poll();
+
+            assert employee != null;
+
+            employee.getTasks().add(task);
+
+            employeePriority.remove(employee);
+            employeePriority.add(employee);
+        }
+    }
+}
